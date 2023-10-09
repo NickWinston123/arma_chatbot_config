ad8d90782a675b0fd260b4978a085721b1beeeb7
NickWinston123 <NickWinston123@gmail.com>
2023-10-09 10:08:02 -0400

    Version 2 using openVPN for CLI interface

---
+++ VPN/addauthkey.py
@@ -0,0 +1,30 @@
+import os
+
+# directory containing the .ovpn 
+vpn_dir = "C:\\Games\\ArmagetronProject2.0\\arma_chatbot_config\\VPN\\OVPN"
+
+# path to the credentials 
+credentials_file = r"C:\\Users\\itsne\\Desktop\\arma_chatbot_config\\VPN\\mycredentials.txt"
+
+
+for filename in os.listdir(vpn_dir):
+    if filename.endswith(".ovpn"):
+        filepath = os.path.join(vpn_dir, filename)
+        
+        with open(filepath, 'r') as f:
+            file_contents = f.readlines()
+        
+        found = False
+        for i, line in enumerate(file_contents):
+            if "auth-user-pass" in line:
+                file_contents[i] = f"auth-user-pass {credentials_file}\n"
+                found = True
+                break
+
+        if not found:
+            file_contents.append(f"auth-user-pass {credentials_file}\n")
+        
+        with open(filepath, 'w') as f:
+            f.writelines(file_contents)
+
+print("All files updated successfully.")
--- manage_chatbot.bat
+++ manage_chatbot.bat
@@ -1,49 +1,78 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+set OPENVPN_PATH="C:\Program Files\OpenVPN\bin\openvpn.exe"
+set REAL_IP=""
+set CONNECTING_TO_VPN=0
 set /a "counter=0"
 
-REM UPDATE
-call :Update
-
 :loop
-REM clear console every 10 iterations
 if !counter! geq 10 (
     cls
     set /a "counter=0"
 )
+
 
 tasklist /FI "IMAGENAME eq armagetronad.exe" 2>NUL | find /I /N "armagetronad.exe">NUL
 if "%ERRORLEVEL%"=="0" (
     echo Armagetronad is running.
 ) else (
     echo Armagetronad is not running.
+    timeout /t 10 > NUL
     call :Update
 )
 
-REM wait 5 seconds before next check
 timeout /t 5 /nobreak > NUL
-
 set /a "counter+=1"
-
 goto loop
 
+:ConnectVPN
+    set CONNECTING_TO_VPN=1
+    taskkill /F /IM openvpn.exe 2>NUL
+    timeout /t 5 > NUL
+
+    set OVPN_DIR=C:\Users\itsne\Desktop\arma_chatbot_config\VPN\OVPN
+    set /a "count=0"
+    for %%f in (%OVPN_DIR%\*.ovpn) do (
+        set /a "count+=1"
+        set "file[!count!]=%%f"
+    )
+
+    set /a "randIndex=(%random% %% count) + 1"
+    set "randFile=!file[%randIndex%]!"
+    start "" %OPENVPN_PATH% --config "!randFile!" > "C:\Users\itsne\Desktop\arma_chatbot_config\openvpn.log" 2>&1
+
+    timeout /t 30 > NUL
+    set CONNECTING_TO_VPN=0
+exit /b
+
+:CheckVPN
+    if "%CONNECTING_TO_VPN%"=="1" (
+        exit /b
+    )
+
+    for /f "tokens=2 delims=: " %%i in ('curl -s https://httpbin.org/ip') do (
+        set CURRENT_IP=%%i
+    )
+
+    ECHO COMPARING %CURRENT_IP% VS %REAL_IP%
+    if "%CURRENT_IP%"=="%REAL_IP%" (
+        echo Not connected to a VPN. Connecting...
+        call :ConnectVPN
+    ) else (
+        echo Connected to a VPN.
+    )
+exit /b
+
 :Update
-
-REM BANNED?
-echo Checking if banned..
-REM file has contents = banned
-for /f %%i in (C:\Users\itsne\AppData\Roaming\Armagetron\var\banned.txt) do (
-    nircmd.exe win activate title "Proton VPN"
-    nircmd.exe win max title "Proton VPN"
-
-    nircmd.exe setcursor 260 445
-    timeout /t 1 > NUL
-    nircmd.exe sendmouse left click    
-    timeout /t 1 > NUL
-   
-    goto IPChecked
-)
+    call :CheckVPN
+    echo Checking if banned..
+    copy /Y "C:\Users\itsne\AppData\Roaming\Armagetron\var\banned.txt" "C:\temp\banned_copy.txt" >NUL 2>&1
+    for /f %%i in (C:\temp\banned_copy.txt) do (
+        echo Banned! Changing IP...
+        call :ConnectVPN
+        goto IPChecked
+    )
 
 :IPChecked
 
